f77bae6da52ba52e44e212ed623771894c7e16ce
vanders <>
2009-10-28 11:30:10 +0000

    Try to un-mangle the stderr/stdout output in the log files.

--- system/build-tools/scripts/auto-build/start.sh
+++ system/build-tools/scripts/auto-build/start.sh
@@ -33,7 +33,7 @@ printf "Update started at %s\n" "`date`"
 # Make sure we have the latest of source & scripts
 echo "Updating sources"
 cd $SOURCE_DIR/syllable
-cvs -z9 -q update -r $CVS_TAG -dP 1>>$LOG 2>&1
+unbuffer cvs -z9 -q update -r $CVS_TAG -dP &>>$LOG
 sync
 
 # Copy sources
